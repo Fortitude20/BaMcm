cebbbc6ad366ded53e725ec0973784793af10324
Ferral Critter <fortitude20@live.com>
2014-03-16 17:59:22 -0400

    Added string overrides to global.settings.php file

--- sites/default/global.settings.php
+++ sites/default/global.settings.php
@@ -1,4 +1,6 @@
 <?php
 $conf['locale_custom_strings_en'][''] = array(
   'Navigation'      => 'Site Menu',
+  'Submitted by !username on !datetime' => 'Written by the amazing !username around !datetime',
+  'Powered by <a href="@poweredby">Drupal</a>' => 'What rhymes with <a href="@poweredby">Drupal</a>',
 );
